c78d3e3c57da4b3ee0f4cb983682c7abe69543a1
Amir Hasan <ahfathi@ce.sharif.edu>
2018-07-31 17:00:50 +0430

    needed urls added

--- management/urls.py
+++ management/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'management'
 
 urlpatterns = [
-    url(r'^new-building$', views.new_building, name='new_building')
+    url(r'^new-building$', views.new_building, name='new_building'),
+    url(r'^register$', views.register, name='register'),
+    url(r'^login$', views.login, name='login'),
 ]
